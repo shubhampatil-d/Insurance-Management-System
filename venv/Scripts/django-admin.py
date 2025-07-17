1c85581f72c42de8d996442cdafb5c1107aea23b
shubhampatil-d <shubhampatil21199@gmail.com>
2025-07-17 20:40:18 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Shubh\Downloads\Insurance_Management_System_in_Django_Framework_With_Source_Code\insurancemanagement-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
